12b31174583915258b79f4841d347f60193219d2
ivan <ivanjl4444@hotmail.com>
2019-05-16 20:58:08 +0200

    test
    meto los test

---
+++ test/examenGIT/testDNI.java
@@ -0,0 +1,19 @@
+package examenGIT;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import examen.DNI;
+import examen.*;
+public class testDNI {
+DNI ivan;
+
+DNI ivandni = new DNI("");
+
+	@Test
+	public void testFormato() {
+		
+	}
+
+}
